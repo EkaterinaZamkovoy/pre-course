187547650299cb3fa488e5e3d6c5a2066711b22b
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2024-05-06 20:00:11 +0300

    HW3.1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -7,7 +7,4 @@ if (number1 % 2 === 0) {
 }
 
 let number = 1;
-console.log(number++);
-console.log(++number);
-console.log(number--);
-console.log(--number);
+console.log(number++ + "," + ++number + "," + number-- + "," + --number);
