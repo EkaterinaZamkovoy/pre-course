b886a399fa778cb426d9b24de07786ed4b079722
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2024-05-17 20:39:40 +0300

    Lesson 2, HW6

---
+++ lesson-02/06.js
@@ -0,0 +1,15 @@
+let passportWithAddress = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "LA",
+  },
+};
+
+let clone2 = structuredClone(passportWithAddress);
+
+clone2.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(clone2.address.city);
